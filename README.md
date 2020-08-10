fb19e8c20b0869c2de11a266c9ab0bc0115a30df
miyajan <miyajan777@gmail.com>
2020-08-11 03:57:14 +0900

    feat: create action

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # action-slack-workflow_run
+
 This action sends the status of the workflow which triggered workflow_run event to Slack.
+
+## Inputs
+
+### `slack-webhook-url`
+
+**Required** The Slack Incoming Webhook URL.
+
+## Example Usage
+
+1. Generate your Slack incoming webhook URL from [here](https://slack.com/apps/A0F7XDUAZ--incoming-webhook-).
+2. Store the URL as [secrets](https://docs.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets) (`SLACK_WEBHOOK_URL` in this example).
+3. Create a workflow file (e.g. `.github/workflows/slack-workflow-run.yml) like below.
+
+```yaml
+on:
+  workflow_run:
+    # "CI" is the name of the workflow whose status will be sent to Slack
+    workflows: ["CI"]
+    types:
+      - completed
+
+jobs:
+  slack:
+    # `conclusion` can be one of `success`, `failure`, `neutral`, `cancelled`, `skipped`, `timed_out`, or `action_required`
+    # send to slack only when the workflow fails
+    if: github.event.workflow_run.conclusion == 'failure'
+    runs-on: ubuntu-latest
+    steps:
+      - uses: miyajan/action-slack-workflow_run
+        with:
+          slack-webhook-url: ${{ secrets.SLACK_WEBHOOK_URL }}
+```
---
+++ src/main.ts
@@ -0,0 +1,117 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { IncomingWebhook } from "@slack/webhook";
+
+type Field = {
+  title: string;
+  value: string;
+  short?: boolean;
+};
+
+type WorkflowRun = {
+  conclusion:
+    | "success"
+    | "failure"
+    | "neutral"
+    | "cancelled"
+    | "skipped"
+    | "timed_out"
+    | "action_required";
+  event: string;
+  head_branch: string;
+  html_url: string;
+};
+
+export async function run(): Promise<void> {
+  try {
+    validateContext();
+
+    const workflowRun: WorkflowRun = github.context.payload.workflow_run;
+    const url = core.getInput("slack-webhook-url", { required: true });
+    const webhook = new IncomingWebhook(url);
+    const fallback = buildFallback(workflowRun);
+    const authorName = "github-actions";
+    const authorIcon = "https://avatars2.githubusercontent.com/in/15368?v=4";
+    const color = buildColor(workflowRun);
+    const fields = buildFields(workflowRun);
+
+    await webhook.send({
+      attachments: [
+        {
+          fallback,
+          author_name: authorName,
+          author_icon: authorIcon,
+          color,
+          fields,
+        },
+      ],
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function validateContext(): void {
+  const context = github.context;
+  if (context.eventName !== "workflow_run") {
+    throw new Error(
+      `This action only supports workflow_run event. (eventName=${context.eventName}`
+    );
+  }
+  if (context.action !== "completed") {
+    throw new Error(
+      `This action only supports completed action. (action=${context.action}`
+    );
+  }
+}
+
+function buildFallback(workflowRun: WorkflowRun): string {
+  const texts = [
+    `Branch: ${workflowRun.head_branch}`,
+    `Event: ${workflowRun.event}`,
+    `Status: ${workflowRun.conclusion}`,
+    `Workflow URL: ${workflowRun.html_url}`,
+  ];
+  return texts.join("\n");
+}
+
+function buildColor(workflowRun: WorkflowRun): string {
+  switch (workflowRun.conclusion) {
+    case "failure":
+    case "timed_out":
+      return "danger";
+    case "success":
+      return "good";
+    default:
+      return "warning";
+  }
+}
+
+function buildFields(workflowRun: WorkflowRun): Field[] {
+  return [
+    {
+      title: "Branch",
+      value: workflowRun.head_branch,
+      short: true,
+    },
+    {
+      title: "Event",
+      value: workflowRun.event,
+      short: true,
+    },
+    {
+      title: "Status",
+      value: workflowRun.conclusion,
+      short: true,
+    },
+    {
+      title: "Workflow URL",
+      value: workflowRun.html_url,
+      short: false,
+    },
+  ];
+}
+
+(async () => {
+  await run();
+})();
